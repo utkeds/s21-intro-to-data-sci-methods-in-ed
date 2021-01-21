ecf4cc569c913a4e99aef1950b9ba0049107729b
Joshua Rosenberg <jrosen@msu.edu>
2021-01-21 13:19:55 -0500

    update site

--- render.R
+++ render.R
@@ -1,6 +1,6 @@
 rmarkdown::render_site()
-# fs::dir_delete("docs")
-# fs::dir_copy("_site", "docs")
-# fs::dir_delete("_site")
+fs::dir_delete("docs")
+fs::dir_copy("_site", "docs")
+fs::dir_delete("_site")
 
 # pagedown::chrome_print(here::here("docs", "index.html"))
