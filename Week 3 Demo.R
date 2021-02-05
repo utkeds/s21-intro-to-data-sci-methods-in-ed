c28681c7227f5e5d5393faae999e70f46fa2221b
Alex Lishinski <alexlishinski@gmail.com>
2021-02-05 17:08:04 -0500

    homework week 3

--- Week 3 Demo.R
+++ Week 3 Demo.R
@@ -8,3 +8,31 @@ library(janitor)
 
 data <- readr::read_csv(here("data", "answer_export.csv"))
 
+colnames(data)
+names(data)
+
+
+data <- data %>%
+  rename(ID_num = id,
+         Response = content,
+         Session_num = session_id,
+         Question_num = question_id)
+
+colnames(data)
+
+table(data$Response)
+tabyl(data$Response)
+
+
+
+data_subset <- data %>%
+  rename(ID_num = id,
+         Response = content,
+         Session_num = session_id,
+         Question_num = question_id) %>%
+  select(Response, Question_num)
+
+
+data_grp <- data_subset %>%
+  group_by(Question_num)
+
