79b4f954c13be1e1e4f82b78274b794a7355b29a
Alex Lishinski <alexlishinski@gmail.com>
2021-03-12 12:31:02 -0500

    homework 8

--- Week 8 demo.R
+++ Week 8 demo.R
@@ -4,8 +4,38 @@ library(skimr)
 library(visdat)
 library(sjPlot)
 library(psych)
-
+library(ggplot2)
+library(dplyr)
 
 data(sci_mo_processed)
 
-summary(sci_mo_processed$total_points_earned)
+View(sci_mo_processed)
+
+vis_dat(sci_mo_processed)
+
+kable(skim(sci_mo_processed))
+
+unique(sci_mo_processed$student_id)
+
+describe(sci_mo_processed)
+
+
+sci_mo_processed %>% select(starts_with("q")) %>%
+  correlate() %>%
+  rplot()
+
+
+sci_mo_processed %>% select(starts_with("q")) %>%
+    describe() %>%
+      ggplot() +
+        geom_col(aes(x= vars, y = sd))
+
+
+fit <- lm(FinalGradeCEMS ~ TimeSpent + Gender + TimeSpent*Gender, data = sci_mo_processed)
+summary(fit)
+
+tab_model(fit)
+
+
+
+
